4edc3589d458d825c1b109089d0f2c9adab1eaa4
nhinnt <nguyenngocthuynhi@gmail.com>
2017-04-24 16:16:51 +0700

    Cap nhap frmHrNhanVien _ Nhi (#10)
    * Cap nhap frmHrNhanVien _ Nhi
    
    * Sua cau truc code HRNhanVien va List...

---
+++ BHXH.HR.Business/HrThangBangLuong.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BHXH.HR.Business
+{
+public    class HrThangBangLuong
+    {
+        public static BHXH.Data.HrThangBangLuong GetTenNgach(string MaNgach)
+        {
+            BHXH.Data.BHXHEntities ctx = new Data.BHXHEntities();
+            var query = from c in ctx.HrThangBangLuong
+                        where c.MaNgach.ToString() == MaNgach.ToString()
+                        select c;
+
+            return query.ToList().First();
+
+        }
+    }
+}
