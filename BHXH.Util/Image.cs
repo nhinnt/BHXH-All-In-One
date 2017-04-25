da14d44bb70cf9fb0a2ca2ab048c3022e957b726
langtudien <langtudien@gmail.com>
2017-04-25 15:07:46 +0700

    update util: Image

---
+++ BHXH.Util/Image.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+namespace BHXH.Util
+{
+    public class Image
+    {
+        public static System.Drawing.Image BinaryToImage(System.Data.Linq.Binary binary)
+        {
+            if (binary.Length > 0)
+            {
+                byte[] b = binary.ToArray();
+                MemoryStream ms = new MemoryStream(b);
+                System.Drawing.Image img = System.Drawing.Image.FromStream(ms);
+                return img;
+            }
+            else return null;
+        }
+    }
+}
