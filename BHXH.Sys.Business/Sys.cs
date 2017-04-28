97757029ed18dcc2d553b4001fc22a513c7a84c0
langtudien <langtudien@gmail.com>
2017-04-28 17:55:59 +0700

    fix lỗi conflict

--- BHXH.Sys.Business/Sys.cs
+++ BHXH.Sys.Business/Sys.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections;
 
 namespace BHXH.Sys.Business
 {
@@ -10,6 +12,21 @@ namespace BHXH.Sys.Business
     {
         public static BHXH.Data.SysUser LoginedUser;
         public static BHXH.Data.SysUserSettings UserSettings;
+
+   
+        public void SaveConnectionString(string Server, string Database, string User, string Password)
+        {
+
+//            string myConnectionString = ConfigurationManager.ConnectionStrings["BHXHEntities"].ConnectionString;
+
+
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var connectionStringsSection = (ConnectionStringsSection)config.GetSection("connectionStrings");
+            connectionStringsSection.ConnectionStrings["BHXHEntities"].ConnectionString = "Data Source="+Server+";Initial Catalog="+Database+";UID="+User+";password="+Password;
+            config.Save();
+            ConfigurationManager.RefreshSection("connectionStrings");
+
+        }
 
     }
 }
